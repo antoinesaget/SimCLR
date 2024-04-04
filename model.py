e6269a817e9fae25e721760830b98b5e8c07cd15
antoinesaget <antoinesaget19@gmail.com>
2024-04-04 20:22:30 +0000

    File cleaning

--- model.py
+++ model.py
@@ -1,16 +1,16 @@
 import os
+
 import torch
 
-from simclr import SimCLR
 from simclr.modules import LARS
 
 
 def load_optimizer(args, model):
-
     scheduler = None
-    if args.optimizer == "Adam":
-        optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)  # TODO: LARS
-    elif args.optimizer == "LARS":
+    if args.optimizer == 'Adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)  # TODO: LARS
+        # optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)  # TODO: LARS
+    elif args.optimizer == 'LARS':
         # optimized using LARS with linear learning rate scaling
         # (i.e. LearningRate = 0.3 × BatchSize/256) and weight decay of 10−6.
         learning_rate = 0.3 * args.batch_size / 256
@@ -18,7 +18,7 @@ def load_optimizer(args, model):
             model.parameters(),
             lr=learning_rate,
             weight_decay=args.weight_decay,
-            exclude_from_weight_decay=["batch_normalization", "bias"],
+            exclude_from_weight_decay=['batch_normalization', 'bias'],
         )
 
         # "decay the learning rate with the cosine decay schedule without restarts"
@@ -32,7 +32,7 @@ def load_optimizer(args, model):
 
 
 def save_model(args, model, optimizer):
-    out = os.path.join(args.model_path, "checkpoint_{}.tar".format(args.current_epoch))
+    out = os.path.join(args.model_path, 'checkpoint_{}.tar'.format(args.current_epoch))
 
     # To save a DataParallel model generically, save the model.module.state_dict().
     # This way, you have the flexibility to load the model any way you want to any device you want.
--- utils/yaml_config_hook.py
+++ utils/yaml_config_hook.py
@@ -1,4 +1,5 @@
 import os
+
 import yaml
 
 
@@ -11,14 +12,14 @@ def yaml_config_hook(config_file):
     # load yaml files in the nested 'defaults' section, which include defaults for experiments
     with open(config_file) as f:
         cfg = yaml.safe_load(f)
-        for d in cfg.get("defaults", []):
+        for d in cfg.get('defaults', []):
             config_dir, cf = d.popitem()
-            cf = os.path.join(os.path.dirname(config_file), config_dir, cf + ".yaml")
+            cf = os.path.join(os.path.dirname(config_file), config_dir, cf + '.yaml')
             with open(cf) as f:
-                l = yaml.safe_load(f)
-                cfg.update(l)
+                loaded_config = yaml.safe_load(f)
+                cfg.update(loaded_config)
 
-    if "defaults" in cfg.keys():
-        del cfg["defaults"]
+    if 'defaults' in cfg.keys():
+        del cfg['defaults']
 
     return cfg
